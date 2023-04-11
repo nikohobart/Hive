227b6e7c8372220773053a83a379f8de6a2d5b0f
Niko Hobart <nikohobart@gmail.com>
2023-04-11 17:50:58 -0400

    benchmark example

--- src/example.py
+++ src/example.py
@@ -1,6 +1,9 @@
+import pyperf
 import numpy as np
 import time
 
+
+np.random.seed(0)
 
 def lineqn_solve(n=10000):
     # Generate a large random matrix
@@ -14,13 +17,12 @@ def lineqn_solve(n=10000):
     x = np.linalg.solve(A, b)
     end_time = time.time()
     
-    print("Time taken for solving the linear equation:", end_time - start_time, "seconds")
+    #print("Time taken for solving the linear equation:", end_time - start_time, "seconds")
     return x
 
 
 def matrix_mult(n=10000):
     # Generate two large random matrices
-    n = 10000
     m = n // 2
     A = np.random.rand(n, m)
     B = np.random.rand(m, m)
@@ -31,13 +33,19 @@ def matrix_mult(n=10000):
     D = np.dot(np.dot(A, B), C)
     end_time = time.time()
 
-    print("Time taken for matrix multiplication:", end_time - start_time, "seconds")
+    #print("Time taken for matrix multiplication:", end_time - start_time, "seconds")
     return D
 
-if __name__ == '__main__':
-    start_time = time.time()
-    value1 = lineqn_solve()
-    value2 = matrix_mult()
+def bench_func(n=10000):
+    print('run')
+    value1 = lineqn_solve(n)
+    value2 = matrix_mult(n)
     
-    print("Output: {} + {} = {}".format(value1, value2, value1+value2))
-    print("Took {} seconds".format(time.time() - start_time))
+    return value1+value2
+    #print("Output: v1 + v2 = {}".format(value1, value2, value1+value2))
+    #print("Took {} seconds".format(time.time() - start_time))
+    
+
+if __name__ == '__main__':
+    runner = pyperf.Runner(processes=1, loops=1) # 4 runs
+    runner.bench_func('linear eqn calcs', bench_func, 15000)
--- src/example_hive.py
+++ src/example_hive.py
@@ -1,11 +1,13 @@
 import numpy as np
+import pyperf
 import time
+
 
 from hive import HiveCore
 
 
+np.random.seed(0)
 hive = HiveCore()
-
 
 @hive.remote(server_port=8080)
 def lineqn_solve(n=10000):
@@ -20,8 +22,9 @@ def lineqn_solve(n=10000):
     x = np.linalg.solve(A, b)
     end_time = time.time()
     
-    print("Time taken for solving the linear equation:", end_time - start_time, "seconds")
+    #print("Time taken for solving the linear equation:", end_time - start_time, "seconds")
     return x
+
 
 @hive.remote(server_port=8081)
 def matrix_mult(n=10000):
@@ -37,27 +40,39 @@ def matrix_mult(n=10000):
     D = np.dot(np.dot(A, B), C)
     end_time = time.time()
 
-    print("Time taken for matrix multiplication:", end_time - start_time, "seconds")
+    #print("Time taken for matrix multiplication:", end_time - start_time, "seconds")
     return D
 
-if __name__ == '__main__':
-    start_time = time.time()
-    future1 = lineqn_solve.remote()
-    print("Thread 1: Future Returned")
 
-    print("Thread 1: Do Other Work Here...")
-    future2 = matrix_mult.remote()
+def bench_func(n=10000):
+    future1 = lineqn_solve.remote(n)
+    future2 = matrix_mult.remote(n)
 
     value1 = future1.get()
-    print("Thread 1: Value 1 Returned:", value1)
-    
     value2 = future2.get()
-    print("Thread 1: Value 2 Returned:", value2)
     
-    print("Output: {} + {} = {}".format(value1, value2, value1+value2))
-    print("Took {} seconds".format(time.time() - start_time))
+    return value1 + value2
 
-    stored_values1 = hive.store.get(future1._object_id)
-    stored_values2 = hive.store.get(future2._object_id)
-    print("Thread 1: Values Stored:", *stored_values1)
-    print("Thread 1: Values Stored:", *stored_values2)
+
+if __name__ == '__main__':
+    runner = pyperf.Runner(processes=1, loops=1) # 4 runs
+    runner.bench_func('parallelized linear eqn calcs', bench_func, 15000)
+    # future1 = lineqn_solve.remote()
+    # print("Thread 1: Future Returned")
+
+    # print("Thread 1: Do Other Work Here...")
+    # future2 = matrix_mult.remote()
+
+    # value1 = future1.get()
+    # print("Thread 1: Value 1 Returned:", value1)
+    
+    # value2 = future2.get()
+    # print("Thread 1: Value 2 Returned:", value2)
+    
+    # print("Output: {} + {} = {}".format(value1, value2, value1+value2))
+    # print("Took {} seconds".format(time.time() - start_time))
+
+    # stored_values1 = hive.store.get(future1._object_id)
+    # stored_values2 = hive.store.get(future2._object_id)
+    # print("Thread 1: Values Stored:", *stored_values1)
+    # print("Thread 1: Values Stored:", *stored_values2)
