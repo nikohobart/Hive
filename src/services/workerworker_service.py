47b5f84fb6588e0d5cef6ac06eb89f05f24c6100
Alexander Du <alexdu2002@gmail.com>
2023-04-11 15:20:08 -0400

    restored deleted files

---
+++ src/services/workerworker_service.py
@@ -0,0 +1,21 @@
+import psutil
+
+from src.proto import workerworker_pb2
+from src.proto import workerworker_pb2_grpc
+from src.utils import serialization
+
+
+class WorkerWorkerService(workerworker_pb2_grpc.WorkerWorkerServiceServicer):
+    def GetObject(self, request, context):
+        print("Worker: GetObject RPC Called")
+
+        depickled_object_ids = serialization.deserialize(request.object_ids)
+
+        print("Worker: Received object ids:", depickled_object_ids)
+
+        # TODO: Implement object getting
+        objects = []
+        
+        # return result to worker
+        print("Worker: Returning objects:", objects)
+        return workerworker_pb2.TaskReply(objects=serialization.serialize(objects))
---
+++ src/utils/future.py
@@ -0,0 +1,26 @@
+import uuid
+
+class Future:
+    _lastId = 0
+
+    def __init__(self):
+        Future._lastId + 1
+        self._object_id = Future._lastId
+        self._result = None
+        self._exception = None
+
+    def set_result(self, result):
+        self._result = result
+    
+    def set_exception(self, exception):
+        self._exception = exception
+    
+    def get(self):
+        while self._result is None and self._exception is None:
+            pass
+        if self._exception:
+            raise self._exception
+        return self._result
+    
+    def done(self):
+        return self._result is not None or self._exception is not None
